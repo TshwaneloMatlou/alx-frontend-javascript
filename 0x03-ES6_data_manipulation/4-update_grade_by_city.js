a282d08b9684d96d371b71766031fdc11e35f185
tshwanelomatlou <tshwanelomatlou55@gmail.com>
2023-10-03 23:22:34 -0700

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,8 @@
-export default function updateStudentGradeByCity (students, city, newGrades) {
-  return students
-    .filter((student) => student.location.localeCompare(city) === 0)
-    .map((x) => {
-      const newGrade = newGrades.filter((y) => y.studentId === x.id);
-      const student = x;
-      if (newGrade.length === 1) student.grade = newGrade[0].grade;
-      else student.grade = 'N/A';
-      return student;
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter(student => student.location === city)
+    .map(student => {
+      let grade = newGrades.find(g => g.studentId === student.id);
+      return { ...student, grade: (grade && grade.grade) || 'N/A' };
     });
 }
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,10 @@
-export default function createInt8TypedArray (length, position, value) {
-  if (position >= length) throw Error('Position outside range');
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view.set([value], position);
-  return new DataView(buffer);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
 }
