c57af3afe7cc23c252e394ccbc8ee1baaa619b59
tshwanelomatlou <tshwanelomatlou55@gmail.com>
2023-10-03 23:19:34 -0700

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
+export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
     return [];
   }
-  return students.map(student => student.id);
+  return arr.map((student) => student.id);
 }
